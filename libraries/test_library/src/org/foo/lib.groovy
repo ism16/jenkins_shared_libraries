50cd24c0add7328eae626c7c64109b9813e94824
ism16 <30345278+ism16@users.noreply.github.com>
2020-11-25 00:21:05 -0800

    Add test library

---
+++ libraries/test_library/src/org/foo/lib.groovy
@@ -0,0 +1,15 @@
+package org.foo;
+
+def print_text(x)
+{
+  echo "${x}"
+}
+return this;
+
+def info(message) {
+    echo "INFO: ${message}"
+}
+
+def warning(message) {
+    echo "WARNING: ${message}"
+}
