4d4a2d4b411a98abcd6402753b68c3d2e4610c86
ism16 <30345278+ism16@users.noreply.github.com>
2020-11-26 13:30:29 -0800

    Added complex pipeline

--- vars/complex_pipeline.groovy
+++ vars/complex_pipeline.groovy
@@ -3,27 +3,37 @@ def call(Map config=[:], Closure body) {
         stage("Checkout") {
             agent {label '${config.node}' }
 
-            git branch: '${config.branch}', credentialsId: '${config.credentials}', url: '${config.repo}'
+            steps{
+
+                git branch: '${config.branch}', credentialsId: '${config.credentials}', url: '${config.repo}'
+
+            }
         }
 
         stage("Build") {
             agent {label '${config.node}' }
 
-            echo "Build your application"
-            echo "Building ${config.name}"
+            steps{
+                echo "Build your application"
+                echo "Building ${config.name}"
+            }
         }      
 
         stage("Test") {
             agent {label '${config.node}' }
 
-            echo "Testing ${config.name}"
+            steps{
+                echo "Testing ${config.name}"
+            }
         }        
         
         stage("Deploy") {
             agent {label '${config.node}' }
 
-            if (config.deploy) {
-                echo "Deploying ${config.name}"
+            steps{
+                if (config.deploy) {
+                    echo "Deploying ${config.name}"
+                }
             }
         }        
         
